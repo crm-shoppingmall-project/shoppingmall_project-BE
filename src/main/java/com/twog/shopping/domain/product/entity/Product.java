109c59783ec9cd5efcd7162c86815728c282e4fb
OliverJoo <jjaemoon@nate.com>
2025-12-04 15:50:58 +0900

    feat: divide application.yml - application-mariadb.yml - application-mysql.yml - modify application.yml

--- src/main/java/com/twog/shopping/domain/product/entity/Product.java
+++ src/main/java/com/twog/shopping/domain/product/entity/Product.java
@@ -1,4 +1,26 @@
 package com.twog.shopping.domain.product.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity(name = "entityProduct")
+@Table(name = "product")
+@Getter
+@Setter
+@ToString
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private String productCategory;
+    private java.sql.Timestamp productRegisted;
+    private Integer productQuantity;
+    private Integer productPrice;
+    private ProductStatus productStatus;
+    private java.sql.Timestamp productUpdated;
+
 }
