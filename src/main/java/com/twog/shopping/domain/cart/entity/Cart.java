4b57e451eeef3ec8bcd390e614c77ce025c6810d
OliverJoo <jjaemoon@nate.com>
2025-12-05 11:06:08 +0900

    feat: 장바구니 엔티티 추가

--- src/main/java/com/twog/shopping/domain/cart/entity/Cart.java
+++ src/main/java/com/twog/shopping/domain/cart/entity/Cart.java
@@ -1,4 +1,31 @@
 package com.twog.shopping.domain.cart.entity;
 
+import com.twog.shopping.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Cart")
+@Getter
+@Setter
+@ToString
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private int cartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<CartItem> cartItems = new ArrayList<>();
+
 }
---
+++ src/main/java/com/twog/shopping/domain/cart/entity/CartItemStatus.java
@@ -0,0 +1,5 @@
+package com.twog.shopping.domain.cart.entity;
+
+public enum CartItemStatus {
+    ACTIVE, REMOVED, UPDATED
+}
