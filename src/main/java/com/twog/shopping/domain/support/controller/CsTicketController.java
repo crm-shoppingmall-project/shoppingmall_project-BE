e3add6ebfc656a687b65c0c52f194720db0a7acc
shin-minhyuk <afs_style@naver.com>
2025-12-10 17:50:39 +0900

    [feat] cs post  관련 formData로 받도록 수정

--- src/main/java/com/twog/shopping/domain/support/controller/CsTicketController.java
+++ src/main/java/com/twog/shopping/domain/support/controller/CsTicketController.java
@@ -2,15 +2,15 @@ package com.twog.shopping.domain.support.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import com.twog.shopping.domain.support.dto.CsTicketReplyRequest;
@@ -32,10 +32,10 @@ public class CsTicketController {
         this.csTicketService = csTicketService;
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public CsTicketResponse createTicket(@AuthenticationPrincipal DetailsUser user,
-                                         @RequestBody CsTicketRequest req) {
+                                         @ModelAttribute CsTicketRequest req) {
         Long memberId = user.getMember().getMemberId();
         return csTicketService.createTicket(req, memberId);
     }
@@ -58,11 +58,11 @@ public class CsTicketController {
         return csTicketService.getTicket(id, memberId, role);
     }
 
-    @PostMapping("/{id}/replies")
+    @PostMapping(value = "/{id}/replies", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public CsTicketReplyResponse createReply(@AuthenticationPrincipal DetailsUser user,
                                               @PathVariable Long id, 
-                                              @RequestBody CsTicketReplyRequest req) {
+                                              @ModelAttribute CsTicketReplyRequest req) {
         Long responderId = user.getMember().getMemberId();
         return csTicketService.createReply(id, responderId, req);
     }
